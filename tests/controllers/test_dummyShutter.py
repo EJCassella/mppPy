fc218ce7f4a170f3eb47e7898a378a8bc5844137
Elena Josephine Cassella <elenacassella@sky.com>
2025-05-21 21:19:20 +0100

    Testing dummyShutter

--- tests/controllers/test_dummyShutter.py
+++ tests/controllers/test_dummyShutter.py
@@ -1,1 +1,17 @@
- 
+import pytest
+import logging
+
+from controllers.dummyShutter import dummyShutter
+
+
+# Test __enter__():
+def test_shutter_opens_if_enabled(caplog: pytest.LogCaptureFixture):
+	caplog.set_level(logging.INFO)
+
+	with dummyShutter(enabled=True):
+		pass
+
+	assert caplog.records[0].message == "Shutter opened."
+
+
+# Test __exit__():
